cd5c6c116b1cbfff6f654d805ffe3bde41c6b0b7
Aidaqilah25 <77595867+Aidaqilah25@users.noreply.github.com>
2022-01-06 22:29:57 +0800

    Delete screens directory

--- screens/readQuran/configuration/typography.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-final Color primaryColor = Color(0xFF26A69A);
-final Color secondaryColor = Color(0xFFA7A4B6);
-final Color whiteColor = Color(0xFFFFFFFF);
-final Color blackColor = Color(0xFF000000);
-final Color greenColor = Color(0xFFF9B090);
--- screens/tajweed/model.dart
+++
@@ -1,6 +0,0 @@
-class Item {
-  String title;
-  String longText;
-  String image;
-  Item({required this.title, required this.longText, required this.image});
-}
